12b7556b169e24b979c94830924a2402cbe8f605
AvetBadalyan <avetbadalyan@gmail.com>
2024-05-25 20:18:04 +0400

    add router for sign up

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth-route.js";
 dotenv.config();
 
 mongoose
@@ -14,6 +15,10 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Serverrrrrrr is running on port 3000");
 });
+
+app.use("./api/auth", authRouter);
