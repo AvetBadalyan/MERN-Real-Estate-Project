539e885ab9e184df5a28bff493d9dcad600e866e
AvetBadalyan <avetbadalyan@gmail.com>
2024-05-25 21:34:09 +0400

    handle errors with middleware

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth-route.js";
+import userRouter from "./routes/user-route.js";
 dotenv.config();
 
 mongoose
@@ -18,7 +19,18 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Serverrrrrrr is running on port 3000");
+  console.log("Server is running on port 3000");
 });
 
+app.use("./api/user", userRouter);
 app.use("./api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
