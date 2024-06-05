53f7aff56919589ad6c1182491782a5676919a45
AvetBadalyan <avetbadalyan@gmail.com>
2024-06-05 12:34:15 +0400

    listing page and functionality

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth-route.js";
 import userRouter from "./routes/user-route.js";
+import listingRouter from "./routes/listing-route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
@@ -26,6 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,8 +6,11 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
       <Header />
@@ -16,11 +19,17 @@ const App = () => {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-};
-export default App;
+}
